1c52f5d3fa57c1ffdb11fbc01f0427526fdc8975
George Park <georgewpark@outlook.com>
2024-07-07 12:55:53 +0100

    prettier update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
   content: ['./int-src/html/**/*.html', './int-src/js/**/*.tsx'],
   theme: {
     fontFamily: {
-      kanit: ['Kanit', 'sans-serif'],
+      kanit: ['Kanit', 'sans-serif']
     },
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
