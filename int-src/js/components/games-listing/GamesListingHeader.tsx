f23f9ff9efaca13106088899226e8da681b84b6a
George Park <georgewpark@outlook.com>
2024-07-13 14:39:29 +0100

    move listing header spacing to container

--- int-src/js/components/games-listing/GamesListingHeader.tsx
+++ int-src/js/components/games-listing/GamesListingHeader.tsx
@@ -2,8 +2,8 @@ import Container from '../shared/Container'
 
 const GamesListingHeader = () => {
   return (
-    <header className='pb-8 pt-12 md:pt-16'>
-      <Container>
+    <header>
+      <Container classList='pb-8 pt-12 md:pt-16'>
         <h1 className='text-center text-3xl text-white md:text-4xl'>
           Free-To-Play Games
         </h1>
