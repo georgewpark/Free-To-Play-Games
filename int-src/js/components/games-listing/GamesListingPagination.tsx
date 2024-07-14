a5d759faf80b39cf84c36f967f0ae41fe0d3b6de
George Park <georgewpark@outlook.com>
2024-07-14 11:56:01 +0100

    pagination refactor

--- int-src/js/components/games-listing/GamesListingPagination.tsx
+++ int-src/js/components/games-listing/GamesListingPagination.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useContext, useMemo } from 'react'
+import { useContext, useMemo } from 'react'
 import GamesListingContext from '../../context/GamesListingContext'
 import { ChevronLeftIcon, ChevronRightIcon } from '../shared/Icons'
 
@@ -11,64 +11,63 @@ const GamesListingPagination = () => {
     [gameItems, resultsPerPage]
   )
 
-  let pageButtons: ReactElement[] = []
-
-  for (let i = 1; i <= totalPages; i++) {
-    const isCurrentPageBtn = currentPage === i
-
-    pageButtons.push(
-      <li
-        key={`page ${i}`}
-        className={`${!isCurrentPageBtn ? `hidden` : 'flex'} md:flex`}
-      >
-        <button
-          className={`h-8 w-8 rounded border border-slate-800 bg-slate-700 font-light leading-none transition-colors hover:bg-slate-800 hover:text-yellow-400 ${
-            isCurrentPageBtn ? `bg-slate-800 !font-medium text-yellow-400` : ''
-          }`}
-          aria-label={`page ${i}`}
-          aria-current={isCurrentPageBtn ? 'page' : false}
-          onClick={() => setCurrentPage(i)}
-        >
-          {i}
-        </button>
-      </li>
-    )
-  }
+  const showPagination = !loading && gameItems.length > 0 && totalPages > 1
 
   return (
     <>
-      {!loading && gameItems.length > 0 && totalPages > 1 && (
+      {showPagination && (
         <nav className='mt-8' aria-label='page navigation'>
           <p className='text-center'>
             Page {currentPage} of {totalPages}
           </p>
-          {pageButtons.length > 1 && (
-            <ul className='mt-3 flex justify-center gap-1.5'>
-              {currentPage > 1 && (
-                <li>
+          <ul className='mt-3 flex justify-center gap-1.5'>
+            {currentPage > 1 && (
+              <li>
+                <button
+                  aria-label={`previous page (page ${currentPage - 1})`}
+                  className={`flex h-8 w-8 rounded border border-slate-800 bg-slate-700 leading-none transition-colors hover:bg-slate-800 hover:text-yellow-400`}
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                >
+                  <ChevronLeftIcon classList='m-auto w-3' />
+                </button>
+              </li>
+            )}
+            {[...Array(totalPages)].map((item, i) => {
+              const pageIndex = i + 1
+              const isCurrentPageBtn = currentPage === pageIndex
+
+              return (
+                <li
+                  key={`page ${pageIndex}`}
+                  className={`${!isCurrentPageBtn ? `hidden` : 'flex'} md:flex`}
+                >
                   <button
-                    aria-label={`previous page (page ${currentPage - 1})`}
-                    className={`flex h-8 w-8 rounded border border-slate-800 bg-slate-700 leading-none transition-colors hover:bg-slate-800 hover:text-yellow-400`}
-                    onClick={() => setCurrentPage(currentPage - 1)}
+                    className={`h-8 w-8 rounded border border-slate-800 bg-slate-700 font-light leading-none transition-colors hover:bg-slate-800 hover:text-yellow-400 ${
+                      isCurrentPageBtn
+                        ? `bg-slate-800 !font-medium text-yellow-400`
+                        : ''
+                    }`}
+                    aria-label={`page ${pageIndex}`}
+                    aria-current={isCurrentPageBtn ? 'page' : false}
+                    onClick={() => setCurrentPage(pageIndex)}
                   >
-                    <ChevronLeftIcon classList='m-auto w-3' />
+                    {pageIndex}
                   </button>
                 </li>
-              )}
-              {[...pageButtons]}
-              {currentPage < totalPages && (
-                <li>
-                  <button
-                    aria-label={`next page (page ${currentPage + 1})`}
-                    className={`h-8 w-8 rounded border border-slate-800 bg-slate-700 leading-none transition-colors hover:bg-slate-800 hover:text-yellow-400`}
-                    onClick={() => setCurrentPage(currentPage + 1)}
-                  >
-                    <ChevronRightIcon classList='m-auto w-3' />
-                  </button>
-                </li>
-              )}
-            </ul>
-          )}
+              )
+            })}
+            {currentPage < totalPages && (
+              <li>
+                <button
+                  aria-label={`next page (page ${currentPage + 1})`}
+                  className={`h-8 w-8 rounded border border-slate-800 bg-slate-700 leading-none transition-colors hover:bg-slate-800 hover:text-yellow-400`}
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                >
+                  <ChevronRightIcon classList='m-auto w-3' />
+                </button>
+              </li>
+            )}
+          </ul>
         </nav>
       )}
     </>
